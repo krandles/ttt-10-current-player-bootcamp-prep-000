47f6d6d43394dfd713dedf31d1ed450d841310e9
Kevin Randles <krandles@gmail.com>
2017-11-04 18:43:20 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,13 @@
 def turn_count(board)
   counter = 0
   board.each do |cell|
-  if cell != " " 
-    counter += 1
+    if cell != " "
+      counter += 1
+    end
   end
   counter
 end
 
 def current_player
-  
+
 end
